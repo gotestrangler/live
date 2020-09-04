8cdb86a83c83bc667b6957c7e2656e489797558b
gotestrangler <gotestrangler@LAPTOP-QBH7HUHV.localdomain>
2020-09-04 09:42:47 +0200

    looking for the whole pipe

--- dense_project/extras/stream2.sh
+++ dense_project/extras/stream2.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+sudo /etc/init.d/nginx restart
+
 INPUT='long.mp4'
 
-ffmpeg                                                      \
-    -fflags                   nobuffer                                         \
-    -err_detect               ignore_err                                       \
-    -i                        $INPUT                                           \
-                                                                               \
-    -max_muxing_queue_size    1024                                             \
-                                                                               \
-    -map                      v:0         -map                      0:a\?:0 \
-    -c:a libfdk_aac -b:a 128k \
-                                                                               \
-    -s                        720x720                                        \
-    -c:v                        libx264                                       \
-    -preset                   fast                                               \
+ffmpeg                                                                          \
+    -fflags                   nobuffer                                          \
+    -err_detect               ignore_err                                        \
+    -i                        $INPUT                                            \
+                                                                                \
+    -max_muxing_queue_size    1024                                              \
+                                                                                \
+    -map                      v:0                                               \
+    -map                      0:a\?:0                                           \
+    -c:a                      aac                                               \
+    -b:a                      128k                                              \
+                                                                                \
+    -s                        720x720                                           \
+    -c:v                      libx264                                           \
+    -preset                   fast                                              \
                                                                                \
     -b:v:0                    1000k                                            \
     -maxrate:v:0              1200k                                            \
@@ -29,17 +33,16 @@ ffmpeg                                                      \
     -dash_segment_type        mp4                                              \
     -remove_at_exit           0                                                \
                                                                                \
-    chunks/flip.mpd
-    #/var/www/data/flip.mpd
+    /var/www/data/manifest.mpd
 
-#cp out2.mpd /var/www/data/mpdtest.mpd
+cat /var/www/data/init-stream0.m4s /var/www/data/chunk-stream0* > /var/www/data/video_only.mp4
+cat /var/www/data/init-stream1.m4s /var/www/data/chunk-stream1* > /var/www/data/audio_only.mp4
 
-#cat /var/www/data/init-stream0.m4s /var/www/data/*.m4s > /var/www/data/flip.mp4
-#cat chunks/init-stream0.m4s chunks/*.m4s > flip.mp4
+ffmpeg -i /var/www/data/video_only.mp4 -i /var/www/data/audio_only.mp4 -c copy /var/www/data/mix.ts 
 
-#wget 172.23.190.178/flip.mp4 --header "Host: denseserver.com"
+wget 172.23.89.214/20/mix.ts --header "Host: denseserver.com"
 
-#./../testProgs/testH264VideoStreamer ekstratest.264
+#./../testProgs/transportStreamer mix.ts
 
     #-window_size              5                                                \
     #-extra_window_size        5                                                \
