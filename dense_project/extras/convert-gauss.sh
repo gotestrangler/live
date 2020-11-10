66bf88fcddc4e4b0c495442de7f175b9c733469f
gotestrangler <gote.strindler@gmail.com>
2020-11-10 11:18:49 +0100

    fakkin fiiiine

--- dense_project/extras/convert-gauss.sh
+++ dense_project/extras/convert-gauss.sh
@@ -1,8 +1,33 @@
 
-INPUT='long.mp4'
+INPUT='jen.mp4'
 
+ ffmpeg -err_detect ignore_err -i $INPUT \
+                -map v:0 \
+                -map v:0 \
+                -map v:0 \
+                -map a:0 \
+                -c:v libx264 -s:v 720x720 -b:v 1000k \
+                -c:a aac -b:a 128k              \
+                -b:v:0                    1000k \
+                -maxrate:v:0              1200k \
+                -bufsize:v:0              1000k \
+                -b:v:1                    4000k \
+                -maxrate:v:1              4800k \
+                -bufsize:v:1              4000k \
+                -b:v:2                    6000k \
+                -maxrate:v:2              7200k \
+                -bufsize:v:2              6000k \
+                -f tee \
+                "[select=\'v:0,a\':f=hls\:hls_flags=split_by_time\:hls_init_time=2\:hls_time=2\:hls_list_size=0\:hls_segment_type=mpegts]chunks/manifes.m3u8|
+                [select=\'v:1,a\':f=hls\:hls_flags=split_by_time\:hls_init_time=2\:hls_time=2\:hls_list_size=0\:hls_segment_type=mpegts]chunks/manifes1.m3u8|
+                [select=\'v:2,a\':f=hls\:hls_flags=split_by_time\:hls_init_time=2\:hls_time=2\:hls_list_size=0\:hls_segment_type=mpegts]chunks/manifes2.m3u8"\
+  
+
+<<<<<<< HEAD
+=======
  ffmpeg -err_detect ignore_err -i $INPUT -max_interleave_delta 0 -max_muxing_queue_size 1024 \
   -c:v libx264 -s:v 720x720 -b:v 1000k\
   -c:a aac -b:a 128k \
-  -f tee -map 0:v -map 0:a "[select=\'v:0,a\':f=hls\:hls_flags=split_by_time\:hls_init_time=2\:hls_time=2\:hls_list_size=0\:hls_segment_type=mpegts]chunks/manifes.m3u8"
+  -f tee -map 0:v -map 0:a "[select=\'v:0,a\':f=hls\:hls_flags=split_by_time\:hls_init_time=2\:hls_time=2\:hls_list_size=0\:hls_segment_type=mpegts]/var/www/data/manifes.m3u8"
+>>>>>>> 35512bf6226f879281a013d36201ce2650a3b2a0
 
--- dense_project/extras/hls_now.sh
+++ dense_project/extras/hls_now.sh
@@ -1,9 +1,27 @@
 
+INPUT='hongkong.avi'
 
-INPUT='long.mp4'
-
- ffmpeg -err_detect ignore_err -i $INPUT 
+ ffmpeg -err_detect ignore_err -i $INPUT -max_interleave_delta 0 -max_muxing_queue_size 1024 \
+                -map v:0 \
+                -map v:0 \
+                -map v:0 \
+                -c:v libx264 -s:v 720x720 -b:v 1000k \
+                -c:a aac -b:a 128k              \
+                -b:v:0                    1000k \
+                -maxrate:v:0              1200k \
+                -bufsize:v:0              1000k \
+                -b:v:1                    4000k \
+                -maxrate:v:1              4800k \
+                -bufsize:v:1              4000k \
+                -b:v:2                    6000k \
+                -maxrate:v:2              7200k \
+                -bufsize:v:2              6000k \
   -f hls -hls_init_time 2 -hls_time 2 -hls_list_size 0 -hls_segment_type mpegts \
-  chunks/manifes.m3u8
+<<<<<<< HEAD
+  /chunks/sikky.m3u8
+
+=======
+  ../../../../../var/www/data/chunks/manifes.m3u8
+>>>>>>> 35512bf6226f879281a013d36201ce2650a3b2a0
 
 
